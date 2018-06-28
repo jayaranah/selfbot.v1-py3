9c4992f10a22d4d38a54cbd06706cb31c0998b62
TEAMSLACKBOT <40662966+TEAMSLACKBOT@users.noreply.github.com>
2018-06-28 13:33:08 +0000

    Update config.py

--- SLACKBOT/linepy/config.py
+++ SLACKBOT/linepy/config.py
@@ -28,10 +28,10 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
     APP_VER     = '8.4.1'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'HelloWorld'
+    SYSTEM_NAME = 'SLACKBOT'
     SYSTEM_VER  = '8.22.17'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
